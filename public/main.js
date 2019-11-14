9cc4f7f15697be1b0577069ec0db88e1a09290ef
Reid Hanson <reidhans@buffalo.edu>
2019-11-13 21:30:53 -0500

    Picture-API

--- public/main.js
+++ public/main.js
@@ -1,7 +1,27 @@
-const getPicture = async () => {
+const getPicAPi = async () => {
   const response = await fetch(
-    "https://sdg-astro-api.herokuapp.com/api/Nasa/apod"
-  );
-};
+    'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+  )
+  const img = await response.json()
+  displayStuff(img.url)
+}
 
-document.addEventListener("DOMContentLoaded", main);
+const displayStuff = img => {
+  document.querySelector('.nasaPic').src = img
+}
+document.addEventListener('DOMContentLoaded', getPicAPi)
+
+//
+
+// const PicAPI = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+
+// const getPicAPI = async () => {
+// const response = await fetch (picAPI)
+// const img = await response.json ()
+// displayStuff (img.url)
+
+// const loadAPI = () =>{
+// getPicAPI ()
+// }
+
+// document.addEventListener('DOMContentLoaded', loadAPI)
